f4e671363d7bea7e3bc0e3e941d0887e430eb6aa
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-07-03 16:12:55 +0200

    a

--- app/Http/Controllers/Folder/NameController.php
+++ app/Http/Controllers/Folder/NameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Folder;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Train;
+// Presumo che la classe carbon indichi il date time
 use Carbon\Carbon;
 
 class NameController extends Controller
